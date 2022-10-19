34b5f99b66f8852985c027e9fe3e3784acea6b18
Eliel Santana <100521650+07111992@users.noreply.github.com>
2022-10-19 20:52:04 -0300

    Organizando script.js

--- script.js
+++ script.js
@@ -1,15 +1,11 @@
 function acender() {
     document.querySelector('.acesa-img').style.display = 'block'
     document.querySelector('.apagada-img').style.display = 'none'
-    document.body.style.backgroundColor = 'white'
-    document.querySelector('.texto').style.color = 'black'
-
 }
 
 function apagar() {
     document.querySelector('.acesa-img').style.display = 'none'
-    document.querySelector('.apagada-img').style.display = 'block'
-    
+    document.querySelector('.apagada-img').style.display = 'block'    
 }
 
 
